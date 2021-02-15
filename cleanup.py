0ac5ba71328176f244643e745b91393c11d3ad6b
fansalon <52781116+fansalon@users.noreply.github.com>
2021-02-15 09:58:10 +0100

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,3 +1,21 @@
+# Copyrigth (C) 2019 Federico Ansaloni
+
+# This file is part of TEspeX.
+
+# TEspeX is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# TEspeX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with TEspeX.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import sys
 import time
 import os
